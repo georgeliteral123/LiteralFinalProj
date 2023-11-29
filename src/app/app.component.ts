ba85cd39c44a94c699258970f9480e42bb627bd1
georgeliteral123 <07107411@dwc-legazpi.edu>
2023-11-29 11:30:30 +0800

    updated dark mode theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './theme.service';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+  isDarkMode: boolean = false;
   title = 'crud';
+
+  constructor(private themeService: ThemeService) {
+  }
+  
+  ngOnInit():void{
+    this.themeService.isDarkMode.subscribe(darkMode => {
+      this.isDarkMode = darkMode;
+    });
+  }
+
+
 }
